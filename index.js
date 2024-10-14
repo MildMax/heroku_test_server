c8ea27382cf8ad2e895fe52a92a08f392b65a68c
MildMax <joseph.s.burns93@gmail.com>
2024-10-14 07:33:54 -0400

    adds GET endpoint + value fetcher to validate prefetch origin hits

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Importing Express
 const express = require('express');
+const path = require('path');
 
 // Creating an instance of Express
 const app = express();
@@ -11,20 +12,32 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the heroku test site');
-  });
+  res.sendFile(path.join(__dirname, '/html/index.html'));
+});
+
+let server_value = 0;
+app.get('/value', (req, res) => {
+  res.send(`{"value": "${server_value}"}`)
+})
+
+app.get('/increment', (req, res) => {
+  server_value += 1;
+  console.log('logging server value: ' + server_value);
+  res.statusCode = 204;
+  res.send();
+})
 
 // Defining the /state endpoint that takes a 'value' parameter
 app.get('/state', (req, res) => {
-    // Extracting the 'value' parameter from the query string
-    const value = req.query.value;
-  
-    // Logging the value to the console
-    console.log(`Received value: ${value}`);
-  
-    // Sending a response back
-    res.send(`Logged value: ${value}`);
-  });
+  // Extracting the 'value' parameter from the query string
+  const value = req.query.value;
+
+  // Logging the value to the console
+  console.log(`Received value: ${value}`);
+
+  // Sending a response back
+  res.send(`Logged value: ${value}`);
+});
 
 // Setting the port
 const PORT = process.env.PORT || 3000;
